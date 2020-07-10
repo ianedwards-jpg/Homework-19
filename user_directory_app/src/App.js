5d007ae4e62e32246b38f3ea09b8761e2b1803ca
Ian Edwards <ian.edward@uconn.edu>
2020-07-10 15:14:41 -0400

    update

--- user_directory_app/src/App.js
+++ user_directory_app/src/App.js
@@ -1,36 +1,67 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import ReactTable from 'react-table-v6'
+import 'react-table-v6/react-table.css'
+//Import Components
+import Wrapper from "./components/Wrapper";
 
 class App extends Component {
 
-  constructor(props)  {
+  constructor(props) {
 
-    super(props); 
+    super(props);
 
     this.state = {
-      posts:  []
+      posts: []
     }
 
   }
 
-  componentDidMount(){
-    const url = "https://jsonplaceholder.typicode.com/posts"; 
+  componentDidMount() {
+    const url = "https://jsonplaceholder.typicode.com/posts";
     fetch(url, {
       method: "GET"
     }).then(response => response.json()).then(posts => {
-      this.setState({posts: posts})
+      this.setState({ posts: posts })
     })
-      
+
   }
 
 
-  render () {
+  render() {
+
+    const columns = [
+      {
+        Header: "User ID",
+        accessor: "userId"
+      },
+      {
+        Header: "ID",
+        accessor: "id"
+      },
+      {
+        Header: "Title",
+        accessor: "title"
+      },
+      {
+        Header: "Content",
+        accessor: "body"
+      }
+    ]
     return (
-      <p>Hello Dingus</p>
+      <Wrapper>
+        <ReactTable
+          columns={columns}
+          data={this.state.posts}
+        >
+        </ReactTable>
+
+      </Wrapper>
+
     );
   }
 
-  
+
 }
 
 export default App;
