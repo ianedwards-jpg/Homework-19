a8a5683468022bb788eb9b77703b3bd08bf5abde
Ian Edwards <ian.edward@uconn.edu>
2020-07-10 15:41:46 -0400

    Update App.js
    Got Table to Render

--- user_directory_app/src/App.js
+++ user_directory_app/src/App.js
@@ -18,36 +18,60 @@ class App extends Component {
   }
 
   componentDidMount() {
-    const url = "https://jsonplaceholder.typicode.com/posts";
+    //const url = "https://jsonplaceholder.typicode.com/posts";
+    const url = "https://gist.githubusercontent.com/rominirani/8235702/raw/a50f7c449c41b6dc8eb87d8d393eeff62121b392/employees.json"
     fetch(url, {
       method: "GET"
     }).then(response => response.json()).then(posts => {
-      this.setState({ posts: posts })
+      this.setState({ posts: posts.Employees })
+      console.log("posts", posts.Employees)
     })
 
   }
 
 
   render() {
+    //Employee Table Headers/Accessors
+const columns = [
+  {
+    Header: "User ID",
+    accessor: "userId"
+  },
+  {
+    Header: "Job Title",
+    accessor: "jobTitleName"
+  },
+  {
+    Header: "First Name",
+    accessor: "firstName"
+  },
+  {
+    Header: "Last Name",
+    accessor: "lastName"
+  },
+  {
+    Header: "Preferred Name",
+    accessor: "preferredFullName"
+  },
+  {
+    Header: "Employee Code",
+    accessor: "employeeCode"
+  },
+  {
+    Header: "Employee Region",
+    accessor: "region"
+  },
+  {
+    Header: "Phone Number",
+    accessor: "phoneNumber"
+  },
+  {
+    Header: "Email",
+    accessor: "emailAddress"
+  }
+]
 
-    const columns = [
-      {
-        Header: "User ID",
-        accessor: "userId"
-      },
-      {
-        Header: "ID",
-        accessor: "id"
-      },
-      {
-        Header: "Title",
-        accessor: "title"
-      },
-      {
-        Header: "Content",
-        accessor: "body"
-      }
-    ]
+
     return (
       <Wrapper>
         <ReactTable
@@ -65,3 +89,26 @@ class App extends Component {
 }
 
 export default App;
+
+
+
+
+// //Example Headers/Accessors 
+// const columns = [
+//   {
+//     Header: "User ID",
+//     accessor: "userId"
+//   },
+//   {
+//     Header: "ID",
+//     accessor: "id"
+//   },
+//   {
+//     Header: "Title",
+//     accessor: "title"
+//   },
+//   {
+//     Header: "Content",
+//     accessor: "body"
+//   }
+// ]
