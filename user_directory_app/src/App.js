6cef1a7efbb92e5750d503fb9b6f15aa442b7bd4
Ian Edwards <ian.edward@uconn.edu>
2020-07-10 12:53:49 -0400

    update
    tested fetch components

--- user_directory_app/src/App.js
+++ user_directory_app/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props)  {
+
+    super(props); 
+
+    this.state = {
+      posts:  []
+    }
+
+  }
+
+  componentDidMount(){
+    const url = "https://jsonplaceholder.typicode.com/posts"; 
+    fetch(url, {
+      method: "GET"
+    }).then(response => response.json()).then(posts => {
+      this.setState({posts: posts})
+    })
+      
+  }
+
+
+  render () {
+    return (
+      <p>Hello Dingus</p>
+    );
+  }
+
+  
 }
 
 export default App;
